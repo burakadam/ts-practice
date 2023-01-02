0ecc19e9bcda011e72f3adb9aa0185ad6183e59a
Burak Erden <burakerden@Buraks-MacBook-Air.local>
2023-01-02 17:31:55 +0300

    add string overload to overloads practice

--- ts-function-overload.ts
+++ ts-function-overload.ts
@@ -4,6 +4,7 @@ interface ICoordinate {
 }
 
 function parseCoordinate(obj: ICoordinate): ICoordinate;
+function parseCoordinate(str: string): ICoordinate;
 function parseCoordinate(x: number, y: number): ICoordinate;
 function parseCoordinate(arg1: unknown, arg2?: unknown): ICoordinate {
   let coord: ICoordinate = {
@@ -11,7 +12,12 @@ function parseCoordinate(arg1: unknown, arg2?: unknown): ICoordinate {
     y: 0,
   };
 
-  if (typeof arg1 === 'object') coord = { ...(arg1 as ICoordinate) };
+  if (typeof arg1 === 'string') {
+    arg1.split(',').forEach((str) => {
+      const [key, value] = str.split(':');
+      coord[key as 'x' | 'y'] = parseInt(value, 10);
+    });
+  } else if (typeof arg1 === 'object') coord = { ...(arg1 as ICoordinate) };
   else coord = { x: arg1 as number, y: arg2 as number };
 
   return coord;
@@ -19,3 +25,4 @@ function parseCoordinate(arg1: unknown, arg2?: unknown): ICoordinate {
 
 console.log(parseCoordinate({ x: 12, y: 13 }));
 console.log(parseCoordinate(13, 14));
+console.log(parseCoordinate('{ x: 122, y: 132 }'));
